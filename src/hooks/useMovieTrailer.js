71ee407b91b3843c2cbcc3411ae8016c6938c86f
Soumyaworks <soumya.banerjee98@gmail.com>
2024-09-19 11:01:26 +0530

    Added GPT custom search for movies and bug fixes

--- src/hooks/useMovieTrailer.js
+++ src/hooks/useMovieTrailer.js
@@ -1,10 +1,13 @@
 import { useEffect } from "react";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { API_OPTIONS } from "../utils/constants";
 import { addTrailerVideo } from "../utils/moviesSlice";
 
 const useMovieTrailer = (movieId) => {
   const dispatch = useDispatch();
+
+  const trailerVideo = useSelector((store) => store.movies.trailerVideo);
+
   const getMovieVideos = async () => {
     const data = await fetch(
       "https://api.themoviedb.org/3/movie/" +
@@ -14,12 +17,12 @@ const useMovieTrailer = (movieId) => {
     );
     const json = await data.json();
 
-    const filterData = json.results.filter((video) => video.type === "Trailer");    // if API gives us multiple trailers
-    const trailer = filterData.length ? filterData[0] : json.results[0];            // just take the first trailer, if present, else take the first video returned
+    const filterData = json.results.filter((video) => video.type === "Trailer");
+    const trailer = filterData.length ? filterData[0] : json.results[0];
     dispatch(addTrailerVideo(trailer));
   };
   useEffect(() => {
-    getMovieVideos();
+    !trailerVideo && getMovieVideos();
   }, []);
 };
 
--- src/utils/constants.js
+++ src/utils/constants.js
@@ -23,3 +23,5 @@ export const SUPPORTED_LANGUAGES = [
   { identifier: "hindi", name: "Hindi" },
   { identifier: "spanish", name: "Spanish" },
 ];
+
+export const OPENAI_KEY = "";
